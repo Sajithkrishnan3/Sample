a738645c98fbba41eecfd521591d70364035fd84
Sajithkrishnan3 <isajithpkrishnans333@gmail.com>
2026-05-28 16:59:39 +0530

    Add duplicate 'Hello world' print statement

--- Practice.java
+++ Practice.java
@@ -3,6 +3,8 @@ class Practice {
  static void World(){
     System.out.println("Hi This is Sajith");
   System.out.println("Hello world");
+  System.out.println("Hello world");
+
 }
     
     public static void main(String[] args) {
