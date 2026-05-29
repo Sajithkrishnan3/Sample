6cf6590d1d362cf9098d20d4bb1913dbe818c541
Sajithkrishnan3 <isajithpkrishnans333@gmail.com>
2026-05-29 18:37:51 +0530

    Add Test3 class with main method and output

---
+++ Test3.java
@@ -0,0 +1,17 @@
+class Test3 {
+
+ static void World(){
+    
+  System.out.println("Test 3 completed");
+  
+
+
+}
+    
+    public static void main(String[] args) {
+        
+
+World(); 
+    }
+    
+}
